3fccca9bca07f480706a7a81e6517e71d337465e
93294 <9329496@mail.ru>
2022-09-22 22:13:23 +0300

    Домашнее задание к уроку Циклы Часть 1

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнеее задание 1");
+        System.out.println("Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nЗадание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nЗадание 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nЗадание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n\nДомашнеее задание 2");
+        System.out.println("Задание 1");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + "год является високосным ");
+        }
+        System.out.println("Задание 2");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nЗадание 3");
+        int j = 1;
+        System.out.print("1 ");
+        for (int i = 2; i <= 512; i = i * 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n\nДомашнеее задание 3");
+        System.out.println("Задание 1");
+        int contribution = 29000; // Вклад
+        int theAmountOfAccumulation = 0; // Cумма накопления
+        for (int i = 1; i <= 12; i++) {
+            theAmountOfAccumulation = theAmountOfAccumulation + contribution;
+            System.out.println("Месяц " + i+ " сумма накоплений равна "+ theAmountOfAccumulation+ " рублей");
+        }
+        System.out.println("Задание 2");
+
+        contribution = 29000; // Вклад
+        theAmountOfAccumulation = 0; // Cумма накопления
+        for (int i = 1; i <= 12; i++) {
+            theAmountOfAccumulation= theAmountOfAccumulation+= theAmountOfAccumulation/100;
+            theAmountOfAccumulation = theAmountOfAccumulation + contribution;
+            System.out.println("Месяц " + i+ " сумма накоплений равна "+ theAmountOfAccumulation+ " рублей");
+        }
+
+    }
+}
